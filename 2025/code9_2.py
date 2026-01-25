8e1773d300d3c70c3eb117dcdbbe1e85be11de62
kavalcio <sarpkavalcioglu@gmail.com>
2026-01-24 21:26:32 -0500

    Fix 9_2

--- 2025/code9_2.py
+++ 2025/code9_2.py
@@ -1,34 +1,37 @@
-f = open('./input9.txt')
+import shapely
+
+f = open("./input9.txt")
 lines = f.readlines()
 
 for i in range(len(lines)):
-  lines[i] = lines[i].replace('\n', '').split(',')
+  lines[i] = lines[i].replace("\n", "").split(",")
   for j in range(len(lines[i])):
     lines[i][j] = int(lines[i][j])
 
 area = 0
 
-edges = []
-# Create list of edges of the valid area
-for i in range(len(lines) - 1):
-  edges.append({ 'x1': lines[i][0], 'x2': lines[i+1][0], 'y1': lines[i][1], 'y2': lines[i+1][1] })
-# Loop around at the end, connecting last vertex with first vertex
-edges.append({ 'x1': lines[-1][0], 'x2': lines[0][0], 'y1': lines[-1][1], 'y2': lines[0][1] })
+vertices = []
+for i in range(len(lines)):
+  row = (float(lines[i][0]), float(lines[i][1]))
+  vertices.append(row)
 
-# def check_intersection(line1, line2):
-  
-  
+polygon_full = shapely.Polygon(vertices)
 
 for i in range(len(lines)):
+  vertex_i = lines[i]
   for j in range(i + 1, len(lines)):
-    new_area = (abs(lines[i][0] - lines[j][0]) + 1) * (abs(lines[i][1] - lines[j][1]) + 1)
-    if new_area > area:
-      # TODO: parse through the list of edges, check whether the 4 edges of this rectangle collide with any of them
-      # If any edge has an orthogonal collision (not at the tips), no match
-      # If no edges have orthogonal collisions, check a random point in the middle. But how do i check that?
-      print('MAX:', i, j, lines[i], lines[j], new_area)
+    vertex_j = lines[j]
+
+    new_area = (abs(vertex_i[0] - vertex_j[0]) + 1) * (abs(vertex_i[1] - vertex_j[1]) + 1)
+    if new_area <= area: continue
+
+    polygon_partial = shapely.Polygon([(vertex_i[0],vertex_j[1]), (vertex_j[0], vertex_j[1]), (vertex_j[0],vertex_i[1]), (vertex_i[0],vertex_i[1]) ])
+    if shapely.within(polygon_partial,polygon_full):
+      print("-->> MAX:", i, j, lines[i], lines[j], new_area)
       area = new_area
 
-print('result:', edges)
+print("result:", area)
 
-# 4758460754 too low
+# 3062848107 too high
+# 3036655402 too high
+# 2973394524 too high
